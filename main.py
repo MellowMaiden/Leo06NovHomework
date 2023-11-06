8c6832eb90db34b8fd3faabb946a37b12e539a5d
MellowMaiden <l2628787@gmail.com>
2023-11-06 18:33:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Leo's Homework 06 November
+print("Hello World")
+def QuadraticFormula1(a,b,c):
+    x=((-b+(b**2-4*a*c)**0.5)/(2*a))
+    return(x)
+print(QuadraticFormula1(1,20,3))
+def QuadraticFormula2(a,b,c):
+    x=((-b-(b**2-4*a*c)**0.5)/(2*a))
+    return(x)
+print(QuadraticFormula2(1,20,3))
